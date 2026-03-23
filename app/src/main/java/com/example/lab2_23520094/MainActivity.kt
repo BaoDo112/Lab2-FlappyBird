1b08e53cec833e4956d630f34b9b988327d7ca84
BaoDo112 <dohoangbao420@gmail.com>
2026-03-23 10:35:22 +0700

    Wrap the `GameView` in the application theme within `MainActivity`.
    The commit includes:
    - Integrated `Lab2_23520094Theme` into the `setContent` block of `MainActivity`.
    - Wrapped the `AndroidView` component with the project's theme to ensure consistent UI styling.

--- app/src/main/java/com/example/lab2_23520094/MainActivity.kt
+++ app/src/main/java/com/example/lab2_23520094/MainActivity.kt
@@ -7,18 +7,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import com.example.lab2_23520094.ui.theme.Lab2_23520094Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidView(
-                factory = { context ->
-                    GameView(context)
-                },
-                modifier = Modifier.fillMaxSize()
-            )
+            Lab2_23520094Theme {
+                AndroidView(
+                    factory = { context ->
+                        GameView(context)
+                    },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
         }
     }
 }
